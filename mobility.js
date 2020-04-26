059b6c537712a0ed0df3c7496cf3aa4efcffc6dc
saadiyahhPrivate <saadiyah@mit.edu>
2020-04-26 17:18:56 -0400

    move world schooling viz to schooling page

--- mobility.js
+++ mobility.js
@@ -10,17 +10,6 @@ window.onload = function() {
       console.error('Error:', error);
   });
 
-  // draw per country/time school restrictions map
-  fetch('data/RestrictionsPerCountry/final_school_restrictions.json')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      drawSchoolRestrictions(data);
-    }).catch((error) => {
-      console.error('Error:', error);
-  });
-
   // draw stringency maps
   fetch('data/RestrictionsPerCountry/final_restrictions_data.json')
     .then((response) => {
@@ -75,112 +64,6 @@ var drawFlightsGraph = function(jsondata) {
   .height(400)
   // .width(600)
   .render();
-};
-
-
-// deliberately kept separate
-var schoolRestrictionsViz, schoolRestrictionKeys, unfilteredSchoolData;
-var schoolRestrictionsTimeout;
-var drawSchoolRestrictions = function(data) {
-  unfilteredSchoolData = data;
-  schoolRestrictionKeys = Object.keys(unfilteredSchoolData);
-  document.getElementById("schoolRestrictionRange").setAttribute("min", 0);
-  document.getElementById("schoolRestrictionRange").setAttribute("max", schoolRestrictionKeys.length - 2);
-
-  schoolRestrictionsViz = new d3plus.Geomap()
-    .topojson("js/countries-50m.json")
-    .select("#worldSchoolRestrictions")
-    .colorScale("S1_School closing")
-    .colorScaleConfig({
-      color: ["white", "lightblue", "navy"],
-      axisConfig: {
-        labels: ['0.0', '1.0', '2.0'],
-        ticks: ['0.0', '1.0', '2.0'],
-        tickFormat: function(d) {
-          if (d == '0.0') { return "not applicable"}
-          else if (d == '1.0') {return 'Recommended'}
-          else {return 'Required';}
-        },
-      }})
-    // .width(600)
-    .height(600)
-    .groupBy("id")  // the column in the data
-    // .loadingMessage("") // clear the annoying laoding message,
-    // // but seems to be causing something funky to happen with zooming
-    // .loadingMessage("")
-    .tooltipConfig({
-          title: function(d) {
-            return d["CountryName"];
-          },
-          tbody: [
-            ["School Closure: ", function(d) {
-              if (d["S1_School closing"] < 1) {
-                return "N/A";
-              } else if (d["S1_School closing"] < 2) {
-                return "Recommended";
-              } else {
-                return "Required";
-              }}],
-            ["date:", function(d) { return d["Date"] }],
-          ]
-        })
-    .topojsonId("id");
-
-  var stepForwardRepeatedly = function() {
-    stepForward();
-
-    if (parseInt(document.getElementById("schoolRestrictionRange").value)
-        < parseInt(document.getElementById("schoolRestrictionRange").max)) {
-      // noticed that smaller timeouts seem to cause the map to go wonky :/
-      schoolRestrictionsTimeout = setTimeout(stepForwardRepeatedly, 500);
-    } else {
-      document.getElementById("SchoolRestrictionDateStepPlay").innerHTML = "Play";
-      clearTimeout(schoolRestrictionsTimeout);
-      schoolRestrictionsTimeout = false;
-    }
-  };
-
-  // TODO: nothing interesting happens until Jan 26th, so data was filtered out for after then
-  var stepForward = function() {
-    var doc_el = document.getElementById("schoolRestrictionRange");
-    var curr_val = parseInt(doc_el.value);
-    var next_val = (curr_val + 1) % (schoolRestrictionKeys.length);
-    doc_el.value = next_val;
-    doc_el.onchange();  // explicit call
-  };
-
-  document.getElementById("SchoolRestrictionDateStepPlay").onclick = function() {
-    if (schoolRestrictionsTimeout) {
-      document.getElementById("SchoolRestrictionDateStepPlay").innerHTML = "Play";
-      clearTimeout(schoolRestrictionsTimeout);
-      schoolRestrictionsTimeout = false;
-    } else {
-      document.getElementById("SchoolRestrictionDateStepPlay").innerHTML = "Pause";
-      stepForwardRepeatedly();
-    }
-  };
-
-  document.getElementById("SchoolRestrictionDateStepForward").onclick = function() {
-    stepForward();
-  };
-
-  document.getElementById("SchoolRestrictionDateStepBack").onclick = function() {
-    var doc_el = document.getElementById("schoolRestrictionRange");
-    var curr_val = parseInt(doc_el.value);
-    var next_val = (curr_val - 1) % (schoolRestrictionKeys.length);
-    doc_el.value = next_val;
-    doc_el.onchange();  // explicit call
-  };
-
-  document.getElementById("schoolRestrictionRange").onchange = function() {
-    var curr_val = document.getElementById("schoolRestrictionRange").value;
-    var date_str = schoolRestrictionKeys[curr_val].toString();
-    var my_date = moment(date_str, 'YYYYMMDD');
-    document.getElementById("SchoolRestrictionDateLabel").innerHTML = my_date.format("MMMM Do YYYY, dddd");
-    schoolRestrictionsViz.data(unfilteredSchoolData[schoolRestrictionKeys[curr_val]]).render();
-  };
-
-  document.getElementById("schoolRestrictionRange").onchange();  // explicit call
 };
 
 var stringencyViz;
--- stateschooling.js
+++ stateschooling.js
@@ -13,7 +13,7 @@ d3.json("data/alex/states-10m.json").then(function(d) {
         ready(d,e);
     })
 })
-    
+
 // d3.defer(d3.json, "data/alex/states-10m.json")
 //     .defer(d3.csv, "data/alex/coronavirus-school-closures-state-levelv4.csv")
 //     .await(ready)
@@ -29,7 +29,7 @@ var margin = {top:50, right:50, bottom:0, left:50},
 
 var state_svg = d3.select("#map.mainpage").append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom); 
+    .attr("height", height + margin.top + margin.bottom);
 
 var moving = false;
 var currentValue = 0;
@@ -61,7 +61,7 @@ var slider = state_svg.append("g")
         .on("start.interrupt", function() { slider.interrupt(); })
         .on("start drag", function() {
             currentValue = d3.event.x;
-            update(x.invert(currentValue)); 
+            update(x.invert(currentValue));
         })
     );
 
@@ -72,7 +72,7 @@ var handle = slider.insert("circle", ".track-overlay")
     .attr("class", "handle")
     .attr("r", 9);
 
-var label = slider.append("text")  
+var label = slider.append("text")
     .attr("class", "label")
     .attr("text-anchor", "middle")
     .text(formatDate(startDate))
@@ -85,16 +85,16 @@ var path = d3.geoPath()
 
 function ready(data, closures) {
         //console.log(data)
-    
+
         var states_dictionary = {};
         for (var s in closures) {
             states_dictionary[closures[s].State] = s
         };
         //console.log(states_dictionary)
-    
+
         var states = topojson.feature(data, data.objects.states).features
         //console.log(states)
-    
+
         state_svg.selectAll(".state")
             .data(states)
             .enter().append("path")
@@ -105,7 +105,7 @@ function ready(data, closures) {
             .attr("day", function(d) {return closures[states_dictionary[d.properties.name]]["StateClosureStartDay"]})
             .style('fill', 'grey')//.StateClosureStartDate});
             .attr("transform", "translate(0," + (100) + ")");
-        
+
         playButton.on("click", function() {
             var button = d3.select(this);
             if (button.text() == "Pause") {
@@ -118,7 +118,7 @@ function ready(data, closures) {
                     timer = setInterval(step, 1000);
                     button.text("Pause");
             }
-        
+
             console.log("Slider moving: " + moving);
         //console.log(closures)
     });
@@ -162,3 +162,121 @@ function update(h) {
             }
         })
 }
+
+// deliberately kept separate
+var schoolRestrictionsViz, schoolRestrictionKeys, unfilteredSchoolData;
+var schoolRestrictionsTimeout;
+var drawSchoolRestrictions = function(data) {
+  unfilteredSchoolData = data;
+  schoolRestrictionKeys = Object.keys(unfilteredSchoolData);
+  document.getElementById("schoolRestrictionRange").setAttribute("min", 0);
+  document.getElementById("schoolRestrictionRange").setAttribute("max", schoolRestrictionKeys.length - 2);
+
+  schoolRestrictionsViz = new d3plus.Geomap()
+    .topojson("js/countries-50m.json")
+    .select("#worldSchoolRestrictions")
+    .colorScale("S1_School closing")
+    .colorScaleConfig({
+      color: ["white", "lightblue", "navy"],
+      axisConfig: {
+        labels: ['0.0', '1.0', '2.0'],
+        ticks: ['0.0', '1.0', '2.0'],
+        tickFormat: function(d) {
+          if (d == '0.0') { return "not applicable"}
+          else if (d == '1.0') {return 'Recommended'}
+          else {return 'Required';}
+        },
+      }})
+    // .width(600)
+    .height(600)
+    .groupBy("id")  // the column in the data
+    // .loadingMessage("") // clear the annoying laoding message,
+    // // but seems to be causing something funky to happen with zooming
+    // .loadingMessage("")
+    .tooltipConfig({
+          title: function(d) {
+            return d["CountryName"];
+          },
+          tbody: [
+            ["School Closure: ", function(d) {
+              if (d["S1_School closing"] < 1) {
+                return "N/A";
+              } else if (d["S1_School closing"] < 2) {
+                return "Recommended";
+              } else {
+                return "Required";
+              }}],
+            ["date:", function(d) { return d["Date"] }],
+          ]
+        })
+    .topojsonId("id");
+
+  var stepForwardRepeatedly = function() {
+    stepForward();
+
+    if (parseInt(document.getElementById("schoolRestrictionRange").value)
+        < parseInt(document.getElementById("schoolRestrictionRange").max)) {
+      // noticed that smaller timeouts seem to cause the map to go wonky :/
+      schoolRestrictionsTimeout = setTimeout(stepForwardRepeatedly, 500);
+    } else {
+      document.getElementById("SchoolRestrictionDateStepPlay").innerHTML = "Play";
+      clearTimeout(schoolRestrictionsTimeout);
+      schoolRestrictionsTimeout = false;
+    }
+  };
+
+  // TODO: nothing interesting happens until Jan 26th, so data was filtered out for after then
+  var stepForward = function() {
+    var doc_el = document.getElementById("schoolRestrictionRange");
+    var curr_val = parseInt(doc_el.value);
+    var next_val = (curr_val + 1) % (schoolRestrictionKeys.length);
+    doc_el.value = next_val;
+    doc_el.onchange();  // explicit call
+  };
+
+  document.getElementById("SchoolRestrictionDateStepPlay").onclick = function() {
+    if (schoolRestrictionsTimeout) {
+      document.getElementById("SchoolRestrictionDateStepPlay").innerHTML = "Play";
+      clearTimeout(schoolRestrictionsTimeout);
+      schoolRestrictionsTimeout = false;
+    } else {
+      document.getElementById("SchoolRestrictionDateStepPlay").innerHTML = "Pause";
+      stepForwardRepeatedly();
+    }
+  };
+
+  document.getElementById("SchoolRestrictionDateStepForward").onclick = function() {
+    stepForward();
+  };
+
+  document.getElementById("SchoolRestrictionDateStepBack").onclick = function() {
+    var doc_el = document.getElementById("schoolRestrictionRange");
+    var curr_val = parseInt(doc_el.value);
+    var next_val = (curr_val - 1) % (schoolRestrictionKeys.length);
+    doc_el.value = next_val;
+    doc_el.onchange();  // explicit call
+  };
+
+  document.getElementById("schoolRestrictionRange").onchange = function() {
+    var curr_val = document.getElementById("schoolRestrictionRange").value;
+    var date_str = schoolRestrictionKeys[curr_val].toString();
+    var my_date = moment(date_str, 'YYYYMMDD');
+    document.getElementById("SchoolRestrictionDateLabel").innerHTML = my_date.format("MMMM Do YYYY, dddd");
+    schoolRestrictionsViz.data(unfilteredSchoolData[schoolRestrictionKeys[curr_val]]).render();
+  };
+
+  document.getElementById("schoolRestrictionRange").onchange();  // explicit call
+};
+
+window.onload = function() {
+  // draw per country/time school restrictions map
+  fetch('data/RestrictionsPerCountry/final_school_restrictions.json')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      drawSchoolRestrictions(data);
+    }).catch((error) => {
+      console.error('Error:', error);
+  });
+};
